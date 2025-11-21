cdc9ff9a5209069fdea6e02644d343ec9e6d97d8
Aradhya Chandrakant Dange <88555012+aradhya24@users.noreply.github.com>
2025-11-21 14:09:34 +0530

    Remove AWS provider installation settings
    Removed provider installation configuration for AWS.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,5 @@
 terraform {
   required_version = ">= 1.0.0"
-
-  provider_installation {
-    filesystem_mirror {
-      path    = "C:/terraform-providers"
-      include = ["hashicorp/aws"]
-    }
-    direct {
-      exclude = ["hashicorp/aws"]
-    }
-  }
 }
 
 provider "aws" {
