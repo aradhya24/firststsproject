cd2c60f57512b81875a685bdfbd61775db2699f9
Aradhya Chandrakant Dange <88555012+aradhya24@users.noreply.github.com>
2025-11-21 14:38:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,5 @@
 # ---------------------------------
-# Security Group for SSH + Tomcat
-# ---------------------------------
-resource "aws_security_group" "allow_ssh_tomcat" {
-  name        = "allow_ssh_tomcat"
-  description = "Allow SSH and Tomcat"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Tomcat"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# ---------------------------------
-# EC2 Instance (with Tomcat install)
+# EC2 Instance (with Tomcat9 install)
 # ---------------------------------
 resource "aws_instance" "my_ec2" {
   ami                    = "ami-0a716d3f3b16d290c"
@@ -41,20 +10,28 @@ resource "aws_instance" "my_ec2" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_ssh_tomcat.id]
 
-user_data = <<-EOF
+  user_data = <<-EOF
 #!/bin/bash
 apt update -y
+
+# Required for tomcat package
+apt install -y software-properties-common
+add-apt-repository universe -y
+apt update -y
+
+# Install Java
 apt install -y openjdk-17-jdk
-apt install -y tomcat10 tomcat10-admin tomcat10-common tomcat10-user
 
-# Create Tomcat webapps folder (fix for Ubuntu AMIs)
-mkdir -p /var/lib/tomcat10/webapps
-chown -R tomcat:tomcat /var/lib/tomcat10/webapps
+# Install Tomcat9 (verified working on this AMI)
+apt install -y tomcat9 tomcat9-admin tomcat9-common
 
-systemctl enable tomcat10
-systemctl start tomcat10
+# Create Tomcat webapps folder
+mkdir -p /var/lib/tomcat9/webapps
+chown -R tomcat:tomcat /var/lib/tomcat9/webapps
+
+systemctl enable tomcat9
+systemctl start tomcat9
 EOF
-
 
   tags = {
     Name = "firststsproject"
